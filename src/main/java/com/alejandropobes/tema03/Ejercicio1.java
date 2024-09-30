06ab20612558fc418d76159ee2d474da9d076f8f
Alejandro Pobes <alepobis@gmail.com>
2024-09-30 20:41:13 +0200

    esto workea¿?

--- src/main/java/com/alejandropobes/tema03/Ejercicio1.java
+++ src/main/java/com/alejandropobes/tema03/Ejercicio1.java
@@ -2,6 +2,7 @@ package com.alejandropobes.tema03;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Hola esto es prueba");
+        System.out.println("Ejercicio1");
+        System.out.println("Ejercicio2");
     }
 }
