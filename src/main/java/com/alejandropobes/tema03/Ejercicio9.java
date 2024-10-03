ed65fdb2f9890148d0b37658c08b8aa11e073b75
Alejandro Pobes <alepobis@gmail.com>
2024-10-03 18:45:46 +0200

    actividades 2 corregido

--- src/main/java/com/alejandropobes/tema03/Ejercicio9.java
+++ src/main/java/com/alejandropobes/tema03/Ejercicio9.java
@@ -1,7 +1,6 @@
 package com.alejandropobes.tema03;
 
 import java.util.Scanner;
-
 public class Ejercicio9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -11,19 +10,19 @@ public class Ejercicio9 {
         System.out.println("Por favor introduce el tamaño de cada lado de tu triangulo");
 
         System.out.println("Primer lado en Cm");
-        lado1 = Float.parseFloat(System.in);
+        lado1 = Float.parseFloat(scanner.nextLine());
         System.out.println("Segundo lado en Cm");
-        lado2 = Float.parseFloat(System.in);
+        lado2 = Float.parseFloat(scanner.nextLine());
         System.out.println("Tercer lado en Cm");
-        lado3 = Float.parseFloat(System.in);
+        lado3 = Float.parseFloat(scanner.nextLine());
         scanner.close();
 
         if (lado1 == lado2 && lado1 == lado3){
             System.out.println("!Tu triangulo es un equilatero¡");
-        } else if (lado1 == lado2) {
-            
+        } else if (lado1 != lado2 && lado1 != lado3 && lado2 != lado3) {
+            System.out.println("!Tu triangulo es un escaleno¡");
+        }else{
+            System.out.println("!Tu triangulo es un isoceles¡");
         }
-
-
     }
 }
