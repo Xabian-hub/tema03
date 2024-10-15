8d3fdff062c01febd217a406634db81e15879b20
Alejandro Pobes <73605642+Xabian-hub@users.noreply.github.com>
2024-10-15 15:47:22 +0200

    ejericios ansi 2-4

---
+++ src/main/java/com/alejandropobes/tema03/ansi/Ejercicio3.java
@@ -0,0 +1,11 @@
+package com.alejandropobes.tema03.ansi;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        String cadena ="";
+        for (int i = 1; i <= 9; i++) {
+            cadena = cadena + i;
+            System.out.printf("%9s\n",cadena);
+        }
+    }
+}
