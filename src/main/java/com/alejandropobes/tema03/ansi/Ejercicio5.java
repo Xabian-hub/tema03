6c5ef6fdee81fdc4043a858077919014e86284e2
Alejandro Pobes <73605642+Xabian-hub@users.noreply.github.com>
2024-10-16 15:54:21 +0200

    ejercicio5

--- src/main/java/com/alejandropobes/tema03/ansi/Ejercicio5.java
+++ src/main/java/com/alejandropobes/tema03/ansi/Ejercicio5.java
@@ -1,4 +1,14 @@
 package com.alejandropobes.tema03.ansi;
 
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        String cadena ="";
+        int colorTexto = 29;
+        /*int colorFondo = 40;*/
+        for (int i = 1; i <= 9; i++) {
+            cadena = cadena + i;
+            colorTexto++;
+            System.out.printf("\u001B[1;"+colorTexto+"m"+"%9s\n"+"\u001B[0m",cadena);
+        }
+    }
 }
